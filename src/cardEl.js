359a998c66e3ade92bb9d1fbc025815cc9750217
huslee <Xvclee2551@gmail.com>
2023-01-09 14:11:02 +0800

    hh

--- src/cardEl.js
+++ src/cardEl.js
@@ -26,3 +26,4 @@ export let CategotyList = (todos) => {
     </>
   );
 };
+  
--- src/categories.js
+++ src/categories.js
@@ -1,14 +1,14 @@
 export const categories = [
-    {
+  {
     id: 1,
     name: "Улс төр",
-    },
-    {
+  },
+  {
     id: 2,
     name: "Нийгэм",
-    },
-    {
+  },
+  {
     id: 3,
     name: "Спорт",
-    },
+  },
 ];
