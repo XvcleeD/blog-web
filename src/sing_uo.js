092645713db394881bc64b145b7384357d73fe0c
XvcleeD <Xvclee255@gmail.com>
2023-01-19 19:14:30 +0800

    fdgdfg

---
+++ src/sing_uo.js
@@ -0,0 +1,19 @@
+import { Button, Card } from "react-bootstrap";
+
+export function SingUp( ) {
+  return(
+    <Card>
+      <Card.Body>
+        <Card.Title>
+          Sing Up
+        </Card.Title>
+        
+        <Input></Input>
+        <Input></Input>
+        <Input></Input>
+        <Button></Button>
+
+      </Card.Body>
+    </Card>
+  )
+}
