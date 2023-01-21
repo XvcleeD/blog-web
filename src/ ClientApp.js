54fd7dd53ef248e2f398466e848add76c588bd43
huslee <Xvclee2551@gmail.com>
2023-01-22 05:22:14 +0800

    gg

--- src/ ClientApp.js
+++ src/ ClientApp.js
@@ -1,19 +1,22 @@
 // import Alert from "react-bootstrap/Alert";
 // import { Link } from "react-router-dom";
 import { Link, Route, Routes, useParams } from "react-router-dom";
+import { BlogPageHome } from "./blog_page";
+import { BlogNav } from "./blog_navbar";
 
 export function ClientApp() {
   return (
     <div>
-      <Link to="/">Home</Link> <Link to="/blog">Blog</Link>
-      <Link to="/admin"> admin</Link>
-      <Link to="/singUp"> sing_up</Link>
+      <BlogNav />
       <Routes>
-        <Route path="/" element={<div>Home page</div>} />
+        <Route path="/" element={<BlogPageHome />} />
         <Route path="/blog" element={<div>Blog list</div>} />
         <Route path="/blog/:slug" element={<SingleBlog />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+      <Link to="/">Home</Link> <Link to="/blog">Blog</Link>
+      <Link to="/admin"> admin</Link>
+      <Link to="/singUp"> sing_up</Link>
     </div>
   );
 }
---
+++ src/blog_navbar.js
@@ -0,0 +1,7 @@
+import { Card } from "react-bootstrap";
+
+export function BlogNav() {
+  return (
+    <Card style={{ width: "100%", height: "50px", background: "gray" }}></Card>
+  );
+}
