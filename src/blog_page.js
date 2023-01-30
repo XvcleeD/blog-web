df6802a146dc1cbf7d3ac66e6ec1214a98ae1f54
XvcleeD <Xvclee255@gmail.com>
2023-01-30 19:11:04 +0800

    dsgs

--- src/blog_page.js
+++ src/blog_page.js
@@ -1,4 +1,4 @@
-import Carousel from "react-bootstrap/Carousel";
+// import Carousel from "react-bootstrap/Carousel";
 // import Img1 from "../public/image/download.jpeg";
 
 export function BlogPageHome() {
